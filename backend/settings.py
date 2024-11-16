1929fc88a21d4d958a1acd34589bd9ae766bb083
rubenpalavacas <rubenyadriaya@gmail.com>
2024-11-16 12:49:53 +0100

    Created participant model for database.

--- backend/settings.py
+++ backend/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'databaseManager'
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'teambuilding',
+        'USER': 'datathon',
+        'PASSWORD': 'datathon2024',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
