24332382673fee95d0932feac7980baa62aef3ee
keigratz <113896749+DidacDV@users.noreply.github.com>
2024-11-17 04:19:05 +0100

    More algorithm upgrades, now "considers" role desired for each user and the roles occupied in each team  (really simple logic i want to sleep)

--- backend/algorithmImplementation/__init__.py
+++ backend/algorithmImplementation/__init__.py
@@ -5,7 +5,7 @@ from backend.Features import Features
 from backend.algorithmImplementation.TeamFormationCalc import TeamFormationSystem
 from backend.algorithmImplementation import LanguageParser
 from backend.algorithmImplementation.algorithmLogic import *
-
+from databaseManager.participantDBController import getAlgorithmDict
 
 @dataclass
 class Participant:
@@ -13,71 +13,9 @@ class Participant:
     features: Features
 
 if __name__ == "__main__":
+    participants = getAlgorithmDict()
     # Create individual Feature objects for each participant
-    features1 = Features(age=23, experience_level="Beginner", hackathons_done=2,
-                         programming_skills={"Python": 4, "JavaScript": 3}, preferred_team_size=4,
-                         preferred_languages=["Python"], preferred_role="Analysis")
-    features2 = Features(age=22, experience_level="Beginner", hackathons_done=3,
-                         programming_skills={"Python": 5, "R": 4}, preferred_team_size=4,
-                         preferred_languages=["Python"],preferred_role="Analysis")
 
-    # New participants
-    features5 = Features(age=55, experience_level="Advanced", hackathons_done=55,
-                         programming_skills={"Java": 3}, preferred_team_size=3,
-                         preferred_languages=["Java"],preferred_role="Analysis")
-    features6 = Features(age=25, experience_level="Intermediate", hackathons_done=1,
-                         programming_skills={"JavaScript": 4, "Python": 5}, preferred_team_size=4,
-                         preferred_languages=["JavaScript", "Python"],preferred_role="Analysis")
-    features7 = Features(age=19, experience_level="Beginner", hackathons_done=2,
-                         programming_skills={"C++": 5, "Python": 3}, preferred_team_size=3, preferred_languages=["C++"], preferred_role="Analysis")
-
-    features3 = Features(age=199, experience_level="Advanced", hackathons_done=2,
-                         programming_skills={"C++": 1}, preferred_team_size=1, preferred_languages=["C++"], preferred_role="Analysis")
-
-    features10 = Features(age=1, experience_level="Intermediate", hackathons_done=2,
-                         programming_skills={"C++": 1}, preferred_team_size=1, preferred_languages=["C++"], preferred_role="Analysis")
-
-
-    features11 = Features(age=20, experience_level="Beginner", hackathons_done=2,
-                         programming_skills={"C++": 4}, preferred_team_size=3, preferred_languages=["C++"], preferred_role="Analysis")
-
-    participants = [
-        Participant(
-            id=uuid.uuid4(),
-            features=features1  # Correct assignment of individual Features
-        ),
-        Participant(
-            id=uuid.uuid4(),
-            features=features11
-        ),
-        Participant(
-            id=uuid.uuid4(),
-            features=features2  # Correct assignment of individual Features
-        ),
-        Participant(
-            id=uuid.uuid4(),
-            features=features5
-        ),
-        Participant(
-            id=uuid.uuid4(),
-            features=features6
-        ),
-        Participant(
-            id=uuid.uuid4(),
-            features=features7
-        ),
-        Participant(
-            id=uuid.uuid4(),
-            features=features3
-        ),
-        Participant(
-            id=uuid.uuid4(),
-            features=features10
-        )
-    ]
-
-    partInteams = {p.id: p.features for p in participants}
-
-    team_system = TeamFormationSystem(partInteams, 4)
+    team_system = TeamFormationSystem(participants, 4)
     formed_teams = team_system.getAllTeams()
     team_system.display_teams(formed_teams)
